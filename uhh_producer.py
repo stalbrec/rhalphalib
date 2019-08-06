09c3153e746abc837876971c7032d29936fb064d
Steffen <steffen.albrecht@desy.de>
2019-08-06 15:55:53 +0200

    added QCD Estimation via ralphabet method

--- uhh_producer.py
+++ uhh_producer.py
@@ -1,7 +1,13 @@
 import rhalphalib as rl
 import numpy as np
 from ROOT import TFile, TH1F
-def uhh_producer(channelConfigs=None,ModelName='UHH_Model',gridHistFileName='/afs/desy.de/user/a/albrechs/xxl/af-cms/UHH2/10_2/CMSSW_10_2_10/src/UHH2/JetMass/Histograms/grid.root'):
+def uhh_producer(configs=None):
+    if('ModelName' not in configs):
+        ModelName='UHH_Model'
+    else:
+        ModelName=configs['ModelName']
+
+    gridHistFileName=configs['gridHistFileName']
 
     print('reading grid for nuisance parameter:')
     gridHistFile=TFile(gridHistFileName,'READ')
@@ -19,9 +25,7 @@ def uhh_producer(channelConfigs=None,ModelName='UHH_Model',gridHistFileName='/af
     print('used variation categories:',ParticleCategories)
     print('X: %s , %i bins'%(gridAxises['x'],len(xBins)))
     print('Y: %s , %i bins'%(gridAxises['y'],len(yBins)))
-    if(channelConfigs == None):
-        print('must specify channel Configurations!')
-        exit(0)
+
 
     model = rl.Model(ModelName)
     gridNuisances=[]
@@ -36,20 +40,51 @@ def uhh_producer(channelConfigs=None,ModelName='UHH_Model',gridHistFileName='/af
     print('CMS_lumi', 'lnN')
     lumi = rl.NuisanceParameter('CMS_lumi', 'lnN')
 
-    for channelName,config in channelConfigs.items():
+    #########
+    #QCD ESTIMATION
+    ptbins = np.array([500, 550, 600, 675, 800, 1200])
+    npt = len(ptbins) - 1
+    # msdbins = np.linspace(40, 201, 24)
+    msdbins = np.linspace(50, 250, 101)
+    nmsd = len(msdbins) - 1
+
+    tf = rl.BernsteinPoly("qcd_pass_ralhpTF",(2,3),['pt','rho'])
+    # here we derive these all at once with 2D array
+    ptpts, msdpts = np.meshgrid(ptbins[:-1] + 0.3 * np.diff(ptbins), msdbins[:-1] + 0.3 * np.diff(msdbins), indexing='ij')
+    rhopts = 2*np.log(msdpts/ptpts)
+    ptscaled = (ptpts - 450.) / (1200. - 450.)
+    rhoscaled = (rhopts - (-6)) / ((-2.1) - (-6))
+    validbins = (rhoscaled >= 0) & (rhoscaled <= 1)
+    rhoscaled[~validbins] = 1  # we will mask these out later
+    tf_params = tf(ptscaled, rhoscaled)
+    #########
+
+    channels=configs['channels']
+
+    if(channels == None):
+        print('must specify channel Configurations!')
+        exit(0)
+
+    for channelName,config in channels.items():
         histLocation=config['histLocation']
-        Variable=config['variable']
-        histDir=config['histDir']
+        if('variable' in config):
+            Variable=config['variable']
+        else:
+            Variable='Mass'
         Samples=config['samples']
 
         if ('regions' not in config) or (len(config['regions'])==0):
             config['regions']=['']
 
         for region in config['regions']:
+            region_suffix= ('_'+region) if len(region) > 0 else ''
+            histDir=config['histDir']+ region_suffix
             regionName=channelName+region
             ch=rl.Channel(regionName)
             model.addChannel(ch)
             for sName in Samples:
+                if('QCD' in sName):
+                    continue
                 sampleType=rl.Sample.SIGNAL if sName==config['signal'] else rl.Sample.BACKGROUND
                 sFile=TFile('%s/%s.root'%(histLocation,sName),'READ')
                 hist=sFile.Get(histDir+'/'+Variable+'_central')
@@ -74,6 +109,37 @@ def uhh_producer(channelConfigs=None,ModelName='UHH_Model',gridHistFileName='/af
 
             ch.setObservation(dataHist)
 
+        #Go to next Sample if QCD is not to be included:
+        if 'QCD' not in Samples:
+            continue
+        # steal observable definition from fail channel
+        # failCh = model['ptbin%dfail' % ptbin]
+        ptbin=np.where(ptbins==float(channelName.split('Pt')[-1]))[0][0]
+        failCh = model[channelName+'fail']
+        obs = failCh.observable
+        print('channelName',channelName)
+        qcdparams = np.array([rl.IndependentParameter('qcdparam_%s_msdbin%d' % (channelName, i), 0) for i in range(nmsd)])
+        initial_qcd = failCh.getObservation().astype(float)  # was integer, and numpy complained about subtracting float from it
+        for sample in failCh:
+            initial_qcd -= sample.getExpectation(nominal=True)
+        if np.any(initial_qcd < 0.):
+            raise ValueError("uh-oh")
+        sigmascale = 10  # to scale the deviation from initial
+        # print(qcdparams)
+        # print(initial_qcd)
+        scaledparams = initial_qcd + sigmascale*np.sqrt(initial_qcd)*qcdparams
+        # fail_qcd = rl.ParametericSample('ptbin%dfail_qcd' % ptbin, rl.Sample.BACKGROUND, obs, scaledparams)
+        fail_qcd = rl.ParametericSample('%sfail_qcd' % channelName, rl.Sample.BACKGROUND, obs, scaledparams)
+        failCh.addSample(fail_qcd)
+        # pass_qcd = rl.TransferFactorSample('ptbin%dpass_qcd' % ptbin, rl.Sample.BACKGROUND, tf_params[ptbin, :], fail_qcd)
+        pass_qcd = rl.TransferFactorSample('%spass_qcd' % channelName, rl.Sample.BACKGROUND, tf_params[ptbin, :], fail_qcd)
+        model[channelName+'pass'].addSample(pass_qcd)
+
+
+        # qcdparams = np.array([rl.IndependentParameter('qcdparam_ptbin%d_msdbin%d' % (ptbin, i), 0) for i in range(nmsd)])
+
+        # model['ptbin%dpass' % ptbin].addSample(pass_qcd)
+
     # import pickle
     # with open("model.pkl", "wb") as fout:
     #     pickle.dump(model, fout)
@@ -85,26 +151,10 @@ def uhh_producer(channelConfigs=None,ModelName='UHH_Model',gridHistFileName='/af
 
 
 if __name__ == '__main__':
-    channels={
-    'TopMass':{
-    'histLocation':'/afs/desy.de/user/a/albrechs/xxl/af-cms/dazsle/JetMass/Histograms/top',
-    'variable':'Mass',
-    'histDir':'JetMass_pt300',
-    'samples':['SingleTop','TTbar','WJets','other'],
-    'signal':'TTbar',
-    'obs':'Pseudo',
-    'varyPseudoLike':'Mass_1_0_chargedH_up'
-    }#,
-    # 'WMass':{
-    # 'histLocation':'/afs/desy.de/user/a/albrechs/xxl/af-cms/dazsle/JetMass/Histograms/W',
-    # 'variable':'Mass',
-    # 'histDir':'JetMass_Nak8_500sel',
-    # 'samples':['QCD','WJetsToQQ'],
-    # 'signal':'WJetsToQQ',
-    # 'obs':'Pseudo',
-    # 'regions':['pass','fail']
-    # }
-    }
-    uhh_producer(channels)
+    import json
+    configs=json.load(open('WMass.json'))
+    uhh_producer(configs)
+    from runFit import runFits
+    runFits([configs['ModelName']])
     # for modelName in ['UHH_Model_0','UHH_Model_1','UHH_Model_2']:
     #     uhh_producer(channels,ModelName=modelName)
