3bb101cf21703ee88581e2d3c09bff8a88fbc377
Steffen <steffen.albrecht@desy.de>
2019-08-07 19:30:12 +0200

    clean up and added capability to rebin in mSD

--- uhh_producer.py
+++ uhh_producer.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 from ROOT import TFile, TH1F
 def uhh_producer(configs=None):
+    RebinMSD=True
     if('ModelName' not in configs):
         ModelName='UHH_Model'
     else:
@@ -46,8 +47,9 @@ def uhh_producer(configs=None):
     ptbins = np.array([500, 550, 600, 675, 800, 1200])
     npt = len(ptbins) - 1
     # msdbins = np.linspace(40, 201, 24)
-    msdbins = np.linspace(50, 250, 101)
+    msdbins = np.linspace(50, 250, 21)
     nmsd = len(msdbins) - 1
+    print(msdbins)
 
     tf = rl.BernsteinPoly("qcd_pass_ralhpTF",(2,3),['pt','rho'])
     # here we derive these all at once with 2D array
@@ -89,10 +91,15 @@ def uhh_producer(configs=None):
                 sampleType=rl.Sample.SIGNAL if sName==config['signal'] else rl.Sample.BACKGROUND
                 sFile=TFile('%s/%s.root'%(histLocation,sName),'READ')
                 hist=sFile.Get(histDir+'/'+Variable+'_central')
+                if(RebinMSD):
+                    hist=hist.Rebin(len(msdbins)-1,hist.GetName()+'_newBinning',msdbins)
                 sample=rl.TemplateSample(ch.name+'_'+sName,sampleType,hist)
                 for (gridNuisance,x,y,category) in gridNuisances:
                     histUp=sFile.Get('%s/%s_%i_%i_%s_up'%(histDir,Variable,x,y,category))
                     histDown=sFile.Get('%s/%s_%i_%i_%s_down'%(histDir,Variable,x,y,category))
+                    if(RebinMSD):
+                        histUp=histUp.Rebin(len(msdbins)-1,histUp.GetName()+'_newBinning',msdbins)
+                        histDown=histDown.Rebin(len(msdbins)-1,histDown.GetName()+'_newBinning',msdbins)
                     sample.setParamEffect(gridNuisance,histUp,histDown)
                     sample.setParamEffect(lumi, 1.027)
                 ch.addSample(sample)
@@ -103,10 +110,12 @@ def uhh_producer(configs=None):
                 histPath=config['varyPseudoLike']
                 if '/' not in histPath:
                     histPath=histDir+'/'+histPath
-                dataHist=dataFile.Get(histPath)
+                dataHist=dataFile.Get(histPath).Rebin(len(msdbins)-1,"",msdbins)
                 dataHist.SetName('Mass_central')
             else:
-                dataHist=dataFile.Get(histDir+'/'+'Mass_central')
+                dataHist=dataFile.Get(histDir+'/'+'Mass_central').Rebin(len(msdbins)-1,"",msdbins)
+            if(RebinMSD):
+                dataHist=dataHist.Rebin(len(msdbins)-1,dataHist.GetName()+'_newBinning',msdbins)
 
             ch.setObservation(dataHist)
 
@@ -114,7 +123,6 @@ def uhh_producer(configs=None):
         if 'QCD' not in Samples:
             continue
         # steal observable definition from fail channel
-        # failCh = model['ptbin%dfail' % ptbin]
         ptbin=np.where(ptbins==float(channelName.split('Pt')[-1]))[0][0]
         failCh = model[channelName+'fail']
         obs = failCh.observable
@@ -126,24 +134,11 @@ def uhh_producer(configs=None):
         if np.any(initial_qcd < 0.):
             raise ValueError("uh-oh")
         sigmascale = 10  # to scale the deviation from initial
-        # print(qcdparams)
-        # print(initial_qcd)
         scaledparams = initial_qcd + sigmascale*np.sqrt(initial_qcd)*qcdparams
-        # fail_qcd = rl.ParametericSample('ptbin%dfail_qcd' % ptbin, rl.Sample.BACKGROUND, obs, scaledparams)
         fail_qcd = rl.ParametericSample('%sfail_qcd' % channelName, rl.Sample.BACKGROUND, obs, scaledparams)
         failCh.addSample(fail_qcd)
-        # pass_qcd = rl.TransferFactorSample('ptbin%dpass_qcd' % ptbin, rl.Sample.BACKGROUND, tf_params[ptbin, :], fail_qcd)
         pass_qcd = rl.TransferFactorSample('%spass_qcd' % channelName, rl.Sample.BACKGROUND, tf_params[ptbin, :], fail_qcd)
         model[channelName+'pass'].addSample(pass_qcd)
-
-
-        # qcdparams = np.array([rl.IndependentParameter('qcdparam_ptbin%d_msdbin%d' % (ptbin, i), 0) for i in range(nmsd)])
-
-        # model['ptbin%dpass' % ptbin].addSample(pass_qcd)
-
-    # import pickle
-    # with open("model.pkl", "wb") as fout:
-    #     pickle.dump(model, fout)
 
     print("ROOT used? ", 'ROOT' in sys.modules)
     model.renderCombine(ModelName)
@@ -155,6 +150,7 @@ if __name__ == '__main__':
     configs=json.load(open(sys.argv[1]))
     uhh_producer(configs)
     from runFit import runFits
-    runFits([configs['ModelName']],configs['pathCMSSW'])
-    # for modelName in ['UHH_Model_0','UHH_Model_1','UHH_Model_2']:
-    #     uhh_producer(channels,ModelName=modelName)
+    if('pathCMSSW' in configs):
+        runFits([configs['ModelName']],configs['pathCMSSW'])
+    else:
+        runFits([configs['ModelName']])
